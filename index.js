b801ca1a861909b3e9ad74cc722d62ccff478b45
Silas <silasishallahan@gmail.com>
2019-03-01 12:28:00 -0800

    finished

--- index.js
+++ index.js
@@ -1,26 +1,72 @@
 'use strict';
 
-function fetchFunction() {
+const STORE = {
+  items: [],
+  displayCount: 3,
+};
+
+function render(){
+  
+  //$('.results-list').empty();
+  $('.results').removeClass('hidden');
+
+  let temp = STORE.items.map(item => {
+    console.log('item.message: ' + item.message);
+    return `<img src="${item.message}">`;
+  });
+
+  $('.results-bin').html(temp);
+  //console.log('render ran');
+  //console.log(STORE.items);
+  //console.log(STORE.displayCount);
+
+}
+
+function htmlTemplate(){
+  //console.log('htmlTemplate ran');
+  let templateHolder = '';
+  for(let i=0; i<STORE.displayCount; i++){
+    templateHolder += `<li><img src=${STORE.items[i]} class="results-img"></li>`;
+  }
+  //console.log(templateHolder);
+  return templateHolder;
+}
+
+function fetchImage() {
   fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(jsonObj => {
-      console.log(jsonObj);
-      displayResult(jsonObj);})
-    .catch(error => alert('You have an error'));
-}
-
-function displayResult(responseJson){
+    .then(result => {
+      console.log(result.message);
+      STORE.items.push(result);
+      render();
+    });
   
-  $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img"img>`);
-
-  $('.results').removeClass('hidden');
 }
 
-function pressTheButton(){
+function updateItems(displayCount){
+  
+  for (let i=0; i<displayCount; i++){
+    STORE.items.push(fetchImage());
+  }
+
+}
+
+function handleSubmission(){
   $('form').submit(event => {
     event.preventDefault();
-    fetchFunction();
+    
+    STORE.displayCount = event.currentTarget.counter.value;
+
+    for(let i = 0; i<STORE.displayCount; i++){
+      fetchImage();
+    }
+    
+    console.log(STORE.items);
   });
 }
 
-$(pressTheButton());
+function main(){
+  handleSubmission();
+}
+
+$(main);
